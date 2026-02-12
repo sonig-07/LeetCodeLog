95fac7c08d02c9ac5f2a8e8a27441b4030c39f5a
SONY GUPTA <140641285+sonig-07@users.noreply.github.com>
2026-02-13 01:25:04 +0530

    Time: 753 ms (58.63%), Space: 18.4 MB (69.72%) - LeetHub

--- 0015-3sum/0015-3sum.py
+++ 0015-3sum/0015-3sum.py
@@ -3,34 +3,38 @@ class Solution:
         nums.sort()
         result = []
         n = len(nums)
-
-        for i in range(n):
-
-            # Skip duplicates
+        
+        i = 0
+        while i < n - 2:
+            
+            # Skip duplicate fixed numbers
             if i > 0 and nums[i] == nums[i - 1]:
+                i += 1
                 continue
-
+            
             left = i + 1
             right = n - 1
-
+            
             while left < right:
                 total = nums[i] + nums[left] + nums[right]
-
+                
                 if total == 0:
                     result.append([nums[i], nums[left], nums[right]])
-
+                    
                     # Skip duplicates
                     while left < right and nums[left] == nums[left + 1]:
                         left += 1
                     while left < right and nums[right] == nums[right - 1]:
                         right -= 1
-
+                    
                     left += 1
                     right -= 1
-
+                
                 elif total < 0:
                     left += 1
                 else:
                     right -= 1
-
+            
+            i += 1
+        
         return result
